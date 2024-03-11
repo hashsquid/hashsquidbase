0616d96d186e3bea9188f6756f2b7c2a065bce7d
hashsquid <162920851+hashsquid@users.noreply.github.com>
2024-03-10 20:09:33 -0400

    Add files via upload

---
+++ find_hash.py
@@ -0,0 +1,27 @@
+import secrets
+import argparse
+from hexbytes import HexBytes
+from web3 import Web3
+
+def get_sum(hb):
+    s = 0
+    for i in hb:
+        s +=  i
+    return s
+
+parser = argparse.ArgumentParser()
+parser.add_argument("guardSum", type=int)
+parser.add_argument("baseHash", type=str)
+args = parser.parse_args()
+
+bh = HexBytes(args.baseHash)
+
+gt = False
+while gt == False:
+    new = secrets.token_hex(32)
+    res = Web3.soliditySha3(['bytes32', 'bytes32'], [bh, '0x'+new])
+    s = get_sum(HexBytes(res))
+    if s <= args.guardSum:
+        print (new)
+        print (s)
+        gt = True
